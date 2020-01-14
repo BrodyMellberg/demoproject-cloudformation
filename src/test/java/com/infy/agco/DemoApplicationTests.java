1a6df436f6255b0b96143e0855cc1af3a0c99af0
BrodyMellberg <brody.mellberg@gmail.com>
2020-01-14 16:37:49 +0530

    import package so autowired might actually work

--- src/test/java/com/infy/agco/DemoApplicationTests.java
+++ src/test/java/com/infy/agco/DemoApplicationTests.java
@@ -7,6 +7,7 @@ import com.infy.agco.controller.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
