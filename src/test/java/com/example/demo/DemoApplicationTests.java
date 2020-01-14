592f1efa0af4f610f5ed0f142ce76543d762314b
BrodyMellberg <brody.mellberg@gmail.com>
2020-01-14 14:38:03 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
+//package com.infy.agco;
 
+import com.infy.agco.DemoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 // @RunWith(JUnit4.class) do I need this
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(class = DemoApplication.class)
 public class DemoApplicationTests {
 
 /*	@Before
